289ccb90ce160c72bcd46dc3c77b7dec8bad7245
Allegra <aukseallegra@gmail.com>
2018-06-26 16:56:08 +0100

    Done.

--- app.rb
+++ app.rb
@@ -2,4 +2,18 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+    erb :new   #first gets "/new" w the form to fill in
+  end
+
+  post '/student' do
+    @student = Student.new(params[:student])
+
+    params[:student][:courses].each do |details|
+      Course.new(details)
+    end
+    @courses = Course.all
+    erb :student  #then shows it in student profile 
+  end
+
 end
